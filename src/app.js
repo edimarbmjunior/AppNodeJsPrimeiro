7c78778c6029ee74ddf5fbbda6428737427c9b35
Edimar <edimarbmjunior@gmail.com>
2018-03-16 18:56:18 -0300

    acerto de rotas

--- src/app.js
+++ src/app.js
@@ -6,18 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title:"Node Store API",
-        version: "0.0.1"
-    });
-});
+//Carrega as Rotas
+const indexRoute = require('../routes/index');
+const productRoute = require('../routes/product');
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', route);
-app.use('/products', create);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
