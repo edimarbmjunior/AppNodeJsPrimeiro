31cb8d6f3287bf3795e26c251090504fdb23b9ff
Edimar <edimarbmjunior@gmail.com>
2018-04-06 21:08:37 -0300

    teste

--- src/mongoose-dados-uri/uri.js
+++
@@ -1,7 +0,0 @@
-//conecta ao banco de dados
-//mongoose.connection('mongodb://nodejsedimar:nodejsedimar@ds117739.mlab.com:17739/nodejsteste');
-const uri = 'mongodb://nodejsedimar:nodejsedimar@ds117739.mlab.com:17739/nodejsteste';
-
-exports.recuperaUri = () => {
-    return uri;
-}
